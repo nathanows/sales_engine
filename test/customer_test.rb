f627b1ec085381a4a078282cd57f0ac93d05feb5
Nathan Owsiany <nowsiany@gmail.com>
2014-11-24 16:24:40 -0700

    added invoice_item class

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -18,5 +18,7 @@ class CustomerTest < Minitest::Test
     assert_equal 'Joey Ondricka', customer.name
     assert_equal '2012-03-27 14:54:09 UTC', customer.created_at
     assert_equal '2012-03-27 14:54:09 UTC', customer.updated_at
+    # assert_instance_of Date, customer.created_at
+    # assert_instance_of Date, customer.updated_at
   end
 end
