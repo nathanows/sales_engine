8cc4b15d09dec5b878383b1d6e2c127acdb54e81
Scott Crawford <scottcrawford03@gmail.com>
2014-11-24 18:56:58 -0700

    added test to customer repo

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -3,13 +3,13 @@ require_relative '../lib/customer_repository'
 
 class CustomerRepositoryTest < Minitest::Test
   def test_it_initializes_entries
-    data1 =  {:id         => 1,
+    data1 =  {:id        => 1,
              :first_name => 'Joey',
              :last_name  => 'Ondricka',
              :created_at => '2012-03-27 14:54:09 UTC',
              :updated_at => '2012-03-27 14:54:09 UTC'
                                                       }
-    data2 =  {:id         => 2,
+    data2 =  {:id        => 2,
              :first_name => 'Mary',
              :last_name  => 'Poppins',
              :created_at => '2012-03-27 14:54:09 UTC',
@@ -22,5 +22,25 @@ class CustomerRepositoryTest < Minitest::Test
     assert_equal 2, customer_repo.data.length
     assert_equal 'Joey', customer_repo.data[0].first_name
     assert_equal 'Mary Poppins', customer_repo.data[1].name
+  end
+
+  def test_that_it_creates_unique_objects
+    data1 =  {:id         => 1,
+              :first_name => 'Joey',
+              :last_name  => 'Ondricka',
+              :created_at => '2012-03-27 14:54:09 UTC',
+              :updated_at => '2012-03-27 14:54:09 UTC'
+                                                        }
+    data2 =  {:id         => 2,
+              :first_name => 'Mary',
+              :last_name  => 'Poppins',
+              :created_at => '2012-03-27 14:54:09 UTC',
+              :updated_at => '2012-03-27 14:54:09 UTC'
+                                                        }
+    entries = [data1,data2]
+    refute_empty entries
+    customer_repo = CustomerRepository.new(entries)
+
+    refute customer_repo.data[0].object_id == customer_repo.data[1].object_id
   end
 end
