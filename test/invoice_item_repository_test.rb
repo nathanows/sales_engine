40a03538b8ca790fce127e1dd7ba03cd0bebd443
Scott Crawford <scottcrawford03@gmail.com>
2014-11-24 18:58:52 -0700

    added tests to invoice and invoice item

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -28,4 +28,29 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 539, invoice_item_repo.data[0].item_id
     assert_equal 3, invoice_item_repo.data[1].quantity
   end
+
+  def test_it_initializes_entries
+    data1 = {:id         => 1,
+             :item_id    => 539,
+             :invoice_id => 1,
+             :quantity   => 5,
+             :unit_price => 13635,
+             :created_at => '2012-03-27 14:54:09 UTC',
+             :updated_at => '2012-03-27 14:54:09 UTC'
+                                                      }
+    data2 = {:id         => 2,
+             :item_id    => 402,
+             :invoice_id => 2,
+             :quantity   => 3,
+             :unit_price => 17307,
+             :created_at => '2012-03-27 14:54:09 UTC',
+             :updated_at => '2012-03-27 14:54:09 UTC'
+                                                      }
+
+    entries = [data1,data2]
+    refute_empty entries
+    invoice_item_repo = InvoiceItemRepository.new(entries)
+
+    refute invoice_item_repo.data[0].object_id == invoice_item_repo.data[1].object_id
+  end
 end
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -25,4 +25,26 @@ class InvoiceRepositoryTest < Minitest::Test
     assert_equal 'shipped', invoice_repo.data[0].status
     assert_equal 69, invoice_repo.data[1].merchant_id
   end
+
+  def test_it_initializes_with_attributes
+    data1 = {:id          => 1,
+             :customer_id => 1,
+             :merchant_id => 26,
+             :status      => 'shipped',
+             :created_at  => '2012-03-25 09:54:09 UTC',
+             :updated_at  => '2012-03-25 09:54:09 UTC',
+                                                        }
+    data2 = {:id          => 2,
+             :customer_id => 2,
+             :merchant_id => 69,
+             :status      => 'shipped',
+             :created_at  => '2012-03-25 09:54:09 UTC',
+             :updated_at  => '2012-03-25 09:54:09 UTC',
+                                                        }
+    entries = [data1,data2]
+    refute_empty entries
+    invoice_repo = InvoiceRepository.new(entries)
+
+    refute invoice_repo.data[0].object_id == invoice_repo.data[1].object_id
+  end
 end
