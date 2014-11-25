2cebe66637279c9b89f88013d0b947041f658241
Nathan Owsiany <nowsiany@gmail.com>
2014-11-25 12:04:56 -0700

    added merchant repo finds

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -8,4 +8,36 @@ class MerchantRepository < Repository
     @data ||= entries.map { |entry| Merchant.new(entry) }
   end
 
+  def find_by_id(id)
+    find_by :id, id
+  end
+
+  def find_by_name(name)
+    find_by :name, name
+  end
+
+  def find_by_created_at(created_at)
+    find_by_date :created_at, created_at
+  end
+
+  def find_by_updated_at(updated_at)
+    find_by_date :updated_at, updated_at
+  end
+
+  def find_all_by_id(id)
+    find_all_by :id, id
+  end
+
+  def find_all_by_name(name)
+    find_all_by :name, name
+  end
+
+  def find_all_by_created_at(created_at)
+    find_all_by_date :created_at, created_at
+  end
+
+  def find_all_by_updated_at(updated_at)
+    find_all_by_date :updated_at, updated_at
+  end
+
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -2,41 +2,94 @@ require_relative 'test_helper'
 require_relative '../lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
-  def test_it_initializes_entries
-    data1 = {:id         => 1,
-            :name       => 'Schroeder-Jerde',
-            :created_at => '2012-03-27 14:53:59 UTC',
-            :updated_at => '2012-03-27 14:53:59 UTC'
-                                                    }
-    data2 = {:id         => 2,
-            :name       => 'Apple',
-            :created_at => '2012-03-27 14:53:59 UTC',
-            :updated_at => '2012-03-27 14:53:59 UTC'
-                                                    }
-    entries = [data1,data2]
-    refute_empty entries
-    merchant_repository = MerchantRepository.new(entries)
-
-    assert_equal 2, merchant_repository.data.length
-    assert_equal 1, merchant_repository.data[0].id
-    assert_equal 'Apple', merchant_repository.data[1].name
+  def data1
+   {:id         => 1,
+    :name       => 'Schroeder-Jerde',
+    :created_at => '2012-03-27 14:53:59 UTC',
+    :updated_at => '2012-03-27 14:53:59 UTC'
+                                            }
   end
 
-  def test_that_it_creates_unique_objects
-    data1 = {:id         => 1,
-            :name       => 'Schroeder-Jerde',
-            :created_at => '2012-03-27 14:53:59 UTC',
-            :updated_at => '2012-03-27 14:53:59 UTC'
-                                                    }
-    data2 = {:id         => 2,
-            :name       => 'Apple',
-            :created_at => '2012-03-27 14:53:59 UTC',
-            :updated_at => '2012-03-27 14:53:59 UTC'
-                                                    }
-    entries = [data1,data2]
-    refute_empty entries
-    merchant_repository = MerchantRepository.new(entries)
+  def data2
+     {:id         => 2,
+      :name       => 'Apple',
+      :created_at => '2012-03-27 14:53:59 UTC',
+      :updated_at => '2012-03-27 14:53:59 UTC'
+                                              }
+  end
 
-    refute merchant_repository.data[0].object_id == merchant_repository.data[1].object_id
+  class InitializeMerchantTest < MerchantRepositoryTest
+    def test_it_initializes_entries
+      entries = [data1,data2]
+      refute_empty entries
+      merchant_repository = MerchantRepository.new(entries)
+
+      assert_equal 2, merchant_repository.data.length
+      assert_equal 1, merchant_repository.data[0].id
+      assert_equal 'Apple', merchant_repository.data[1].name
+    end
+
+    def test_that_it_creates_unique_objects
+      entries = [data1,data2]
+      refute_empty entries
+      merchant_repository = MerchantRepository.new(entries)
+
+      refute merchant_repository.data[0].object_id == merchant_repository.data[1].object_id
+    end
+  end
+
+  def new_obj
+      CSVParser.parse('merchants.csv', CSVParser::TEST)
+  end
+
+  class FindMerchantTest < MerchantRepositoryTest
+    def test_it_creates_a_valid_object
+      assert_instance_of MerchantRepository, new_obj
+      assert_equal 100, new_obj.data.length
+    end
+
+    def test_it_can_return_first_instance_of_id
+      find_results = new_obj.find_by_id(1)
+      assert_instance_of Merchant, find_results
+      assert_equal 1, find_results.id
+    end
+
+    def test_it_can_return_all_instances_of_id
+      find_results = new_obj.find_all_by_id(1)
+      assert_equal 1, find_results.length
+    end
+
+    def test_it_can_return_first_instance_of_name
+      find_results = new_obj.find_by_name("Williamson Group")
+      assert_instance_of Merchant, find_results
+      assert_equal 5, find_results.id
+    end
+
+    def test_it_can_return_all_instances_of_name
+      find_results = new_obj.find_all_by_name("Williamson Group")
+      assert_equal 2, find_results.length
+    end
+
+    def test_it_can_return_first_instance_of_created_at
+      find_results = new_obj.find_by_created_at("2012-03-27")
+      assert_instance_of Merchant, find_results
+      assert_equal 1, find_results.id
+    end
+
+    def test_it_can_return_all_instances_of_created_at
+      find_results = new_obj.find_all_by_created_at("2012-03-27")
+      assert_equal 100, find_results.length
+    end
+
+    def test_it_can_return_first_instance_of_updated_at
+      find_results = new_obj.find_by_updated_at("2012-03-27")
+      assert_instance_of Merchant, find_results
+      assert_equal 1, find_results.id
+    end
+
+    def test_it_can_return_all_instances_of_updated_at
+      find_results = new_obj.find_all_by_updated_at("2012-03-27")
+      assert_equal 100, find_results.length
+    end
   end
 end
